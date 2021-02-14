15a901fe16fd4323f1ecfa96bf07c45a10f3662a
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2021-02-14 13:39:04 +0100

    Some text

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.uix.widget import Widget
 
-
 class MenuWindow(Screen):
+    pass
+
+class IntroWindow(Screen):
+    pass
+
+class StatisWindow(Screen):
     pass
 
 class MyApp(App):
     def build(self):
-        return MenuWindow()
+        return StatisWindow()
 
 if __name__ == '__main__':
     MyApp().run()
